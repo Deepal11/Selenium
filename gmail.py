e5b16355a88d4a5212cdc20ae401892e0c948ee9
Deepal11 <64477392+Deepal11@users.noreply.github.com>
2020-06-01 17:20:55 +0530

    Update gmail.py

--- gmail.py
+++ gmail.py
@@ -1,10 +1,10 @@
 from selenium import webdriver
 driver = webdriver.Chrome()
 driver.get("http://gmail.com")
-driver.find_element_by_name("identifier").send_keys('deepaljain11@gmail.com')
+driver.find_element_by_name("identifier").send_keys('your_email_id')
 driver.find_element_by_xpath("//*[@id='identifierNext']/span/span").click()
 driver.implicitly_wait(10)
-driver.find_element_by_name("password").send_keys('Deepal_123')
+driver.find_element_by_name("password").send_keys('your_password')
 driver.find_element_by_xpath("//*[@id='passwordNext']/span/span").click()
 driver.implicitly_wait(10)
 emails = driver.find_elements_by_class_name('zA')
